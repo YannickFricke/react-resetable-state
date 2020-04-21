993f39a092b29d810d2d67bd17edc59ec4fe7888
Yannick Fricke <yannickfricke@googlemail.com>
2020-04-21 22:53:18 +0200

    Added the useResetableState hook

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import React, { useState, SetStateAction } from 'react';
+
+export const useResetableState = <T>(initialState: T): [
+    T,
+    () => void,
+    React.Dispatch<SetStateAction<T>>,
+] => {
+    const [state, setState] = useState<T>(initialState);
+
+    const resetState = () => setState(initialState);
+
+    return [
+        state,
+        resetState,
+        setState,
+    ];
+}
